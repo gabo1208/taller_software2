e83122037289d3c4b079eef720ac92b98e334006
Gabo <gabohd@gmail.com>
2015-01-19 23:16:00 -0430

    Limpiado de commit anterior

--- taller2/reservacion.py
+++ taller2/reservacion.py
@@ -76,5 +76,4 @@ def reservacion(inidate, findate, tarifa):
     aqui los casos de prueba
     cambio coment a verr 
     '''
-    print("hola que ladill")
 reservacion(datetime(2010, 5, 25, 19), datetime(2010, 5, 26, 18, 15),{'diurna': Decimal(10), 'nocturna': Decimal(15)})
